e4c83493596f74bea1baf61f1544fd09352cc714
ccoder74 <80149066+ccoder74@users.noreply.github.com>
2023-08-20 14:30:49 +0200

    Added: reciprocal.py

---
+++ reciprocal.py
@@ -0,0 +1,8 @@
+"""
+reciprocal.py:
+    Returns reciprocal of user input number.
+"""
+
+if __name__ == '__main__':
+    num = float(input('\nEnter a natural number: '))
+    print('The reciprocal of', num, 'is', 1/num, '\n')
